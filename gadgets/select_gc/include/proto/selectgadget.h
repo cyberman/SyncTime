6efcdc911e27f22df65b59439bb3fce41821048c
matjam <chrome@stupendous.net>
2026-01-28 10:33:37 -0800

    chore: remove accidentally added gadgets folder

--- gadgets/select_gc/include/proto/selectgadget.h
+++
@@ -1,7 +0,0 @@
-#ifndef PROTO_SELECTGADGET_H
-#define PROTO_SELECTGADGET_H
-#include <exec/types.h>
-extern struct Library *SelectGadgetBase;
-#include <clib/selectgadget_protos.h>
-#include <pragmas/selectgadget_pragmas.h>
-#endif
